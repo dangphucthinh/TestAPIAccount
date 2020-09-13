381544dd446c9aab57ac11305a285b587fbb8cab
dangphucthinh <dangphucthinha9@gmail.com>
2020-09-13 10:21:23 +0700

    refresh token

---
+++ OAuthCustomeTokenProvider.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.IO;
+using WebApplication2.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplication2
+{
+    public class OAuthCustomeTokenProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //return Task.Factory.StartNew(() =>
+            //{
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            //var userName = context.UserName;
+            //var password = context.Password;
+            //var userService = new UserService();
+            //var user = userService.Validate(userName, password);
+            var user = await manager.FindAsync(context.UserName, context.Password);
+            if (user != null)
+            {
+                var claims = new List<Claim>()
+            {
+                new Claim("Username", user.UserName),
+                new Claim("Email", user.Email),
+                new Claim("FirstName", user.FirstName),
+                new Claim("LastName", user.LastName),
+                new Claim("LoggedOn", DateTime.Now.ToString())
+            };
+                //        foreach (var role in user.Roles)
+                //            claims.Add(new Claim(ClaimTypes.Role, role));
+
+                var data = new Dictionary<string, string>
+            {
+                { "UserName", user.UserName },
+                    {"Email", user.Email },
+                    {"FirstName", user.FirstName },
+                    {"LastName", user.LastName },
+                    
+                //{ "roles", string.Join(",", user.Roles)}
+            };
+                var properties = new AuthenticationProperties(data);
+
+                ClaimsIdentity oAuthIdentity = new ClaimsIdentity(claims,
+                    Startup.OAuthOptions.AuthenticationType);
+
+                var ticket = new AuthenticationTicket(oAuthIdentity, properties);
+                context.Validated(ticket);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Either email or password is incorrect");
+            }
+
+            //});
+        }
+        public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            var newIdentity = new ClaimsIdentity(context.Ticket.Identity);
+
+            var newTicket = new AuthenticationTicket(newIdentity, context.Ticket.Properties);
+            context.Validated(newTicket);
+
+            return Task.FromResult<object>(null);
+        }
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            if (context.ClientId == null)
+                context.Validated();
+
+            return Task.FromResult<object>(null);
+        }
+
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+            return Task.FromResult<object>(null);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
@@ -11,21 +12,36 @@ namespace WebApplication2
 {
     public class Startup
     {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             app.UseCors(CorsOptions.AllowAll);
 
-            OAuthAuthorizationServerOptions option
-                 = new OAuthAuthorizationServerOptions
-                 {
-                     TokenEndpointPath = new PathString("/token"),
-                     Provider = new ApplicationOAuthProvider(),
-                     AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
-                     AllowInsecureHttp = true
-                 };
-            app.UseOAuthAuthorizationServer(option);
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            //OAuthAuthorizationServerOptions option
+            //     = new OAuthAuthorizationServerOptions
+            //     {
+            //         TokenEndpointPath = new PathString("/token"),
+            //         Provider = new ApplicationOAuthProvider(),
+            //         AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
+            //         AllowInsecureHttp = true,
+            //         RefreshTokenProvider = 
+            //     };
+            //app.UseOAuthAuthorizationServer(option);
+            //app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+
+            //
+            double tokenTimeout = double.Parse(ConfigurationManager.AppSettings["tokenTimeout"].ToString());
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new OAuthCustomeTokenProvider(), // We will create
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(tokenTimeout),
+                AllowInsecureHttp = true,
+                RefreshTokenProvider = new OAuthCustomRefreshTokenProvider() // We will create
+            };
+            app.UseOAuthBearerTokens(OAuthOptions);
         }
     }
 }
